b94b85e5ee7a5e2eddea0179cb38aa9bf007083f
tim-ming <tim.ming@icloud.com>
2025-09-07 04:06:14 +0800

    wip: add prettier

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+export default {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  printWidth: 120,
+  tabWidth: 2,
+};
