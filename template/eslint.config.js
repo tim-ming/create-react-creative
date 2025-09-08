5ac091ac9d6273d4f5713ced085a1f43658772d8
tim-ming <tim.ming@icloud.com>
2025-09-08 21:43:51 +0800

    wip: update eslint

--- template/eslint.config.js
+++ template/eslint.config.js
@@ -1,28 +1,40 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 import { defineConfig } from 'eslint/config';
 import prettier from 'eslint-plugin-prettier';
 import prettierConfig from 'eslint-config-prettier';
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { globalIgnores } from 'eslint/config';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['src/**/*.{ts,tsx,js,jsx}', '*.js'],
-    plugins: { js, prettier },
-    extends: ['js/recommended', tseslint.configs.recommended, prettierConfig],
+    files: ['**/*.{ts,tsx,js}'],
+    plugins: { prettier },
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+      prettierConfig,
+    ],
     languageOptions: {
-      globals: globals.node,
+      ecmaVersion: 2022,
+      globals: globals.browser,
+      // Use tsconfig of root for the project
       parserOptions: {
         tsconfigRootDir: __dirname,
       },
     },
+    // configure to your needs
     rules: {
+      'prettier/prettier': 'error',
       '@typescript-eslint/no-unused-vars': [
         'error',
         {
