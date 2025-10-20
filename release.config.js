50116b09da28a296242a42dff5aba152c7252b50
tim-ming <tim.ming@icloud.com>
2025-10-20 18:01:44 +0800

    build(release): make deps-template bump to cause patch release

--- release.config.js
+++ release.config.js
@@ -9,4 +9,16 @@ export default {
       prerelease: true,
     },
   ],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+        releaseRules: [{ type: 'chore', scope: 'deps-template', release: 'patch' }],
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+  ],
 };
